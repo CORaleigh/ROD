fee7adb2b2847300c78c060005498da72795cf3f
xfinger <xfinger@gmail.com>
2015-02-12 17:50:14 -0500

    updates for parking

--- app/permit_chunk.rb
+++ app/permit_chunk.rb
@@ -1,11 +1,11 @@
 # permit_chunk.rb
-# This script is for bulk uploading permit data to Socrata. It is for starting a new Permit data-set or for when 
+# This script is for bulk uploading permit data to Socrata. It is for starting a new Permit data-set or for when
 # you need to push large (~10,000) records.
 # Run this script => ruby permit_chunk.rb followed by an integer representing the 1st permit id you want to upload.
 # EX. ruby permit_chunk.rb 1000  => will upload permit data with permit numbers 1000 through 11000.
 # This will query the database, concatenate the address fields, rename the column names for a more human readable table,
-# adjust the date fields and push up to 10,000 records at a time based on permit id.
- 
+# adjust the date fields and push up to 10,000 records at a time based on permit i
+require 'ruby-oci8'
 require 'net/https'
 require 'hashie'
 require 'rubygems'
@@ -23,9 +23,9 @@ require_relative '../lib/helpers.rb'
 DB = Sequel.oracle( :database => configatron.db, :host => configatron.host, :port => 1531, :user => configatron.user, :password => configatron.pass)
 
 class ConnectQuery
-  
+
   DATE_FORMAT = '%m/%d/%Y'
-  
+
   def initialize(db, min)
     @client = SODA::Client.new({
    :domain => 'data.raleighnc.gov',
@@ -34,9 +34,9 @@ class ConnectQuery
    :password => configatron.client_pass,
    :content_type => 'text/plain',
    :mime_type => 'JSON',
-   :ignore_ssl => true 
-      }) 
-    
+   :ignore_ssl => true
+      })
+
     @db = db
     @view_id = 'hk3n-ieai'   #permit data-set code for Socrata
     @payload =[]
@@ -50,27 +50,27 @@ class ConnectQuery
      @sql = get_sql(@min, @max)
      export
   end
-  
-  
+
+
   def get_sql(min, max)
-      <<-SQL 
+      <<-SQL
       SELECT "PERM_GROUPS"."DEVPLAN_DEVPLAN_NAME", "PERM_GROUPS"."GRP_ISSUE_DATE", "PERM_GROUPS"."GRP_PROPOSED_WORK", "PERM_BLDG"."PERM_WORK_TYPE_CODE_DESCR", "PERM_GROUPS"."STREET_NUM", "PERM_GROUPS"."DIR_PRE", "PERM_GROUPS"."STREET_NAME", "PERM_GROUPS"."STREET_TYPE", "PERM_BLDG"."PERMIT_NUM", "PERM_BLDG"."PERM_SQ_FT_FOR_FEE", "PERM_GROUPS"."GRP_BLDG_NUM_STORIES", "PERM_GROUPS"."NCPIN", "PERM_BLDG"."PERM_COST_OF_CONSTRUCTION", "PERM_GROUPS"."GRP_CNTY_PARC_OWNER_NAME", "PERM_BLDG"."PERM_CONTRACTOR_NAME", "PERM_GROUPS"."RPID_LOT", "PERM_GROUPS"."PERM_C_BLDG_CO_DATE", "PERM_BLDG"."PERM_AUTHORIZED_WORK", "PERM_GROUPS"."GRP_B_NUM_DWEL_UNITS_TOTAL", "PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE_DESCR", "PARCELS"."CNTY_ACCT_NUM", "PERM_GROUPS"."GRP_STATUS", "PERM_GROUPS"."STREET_SUITE", "PARCELS"."PARC_COUNTY", "CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS_DESCR", "PERM_GROUPS"."DIR_SUF", "ADDRESSES"."CITY", "ADDRESSES"."STATE", "ADDRESSES"."ZIP", "LAND_USE_CODES"."LANDUSECODE_DESCR"
       FROM   ((((("IRIS"."PERM_GROUPS" "PERM_GROUPS" INNER JOIN "IRIS"."PERM_BLDG" "PERM_BLDG" ON ("PERM_GROUPS"."GROUP_NUM"="PERM_BLDG"."GROUP_NUM") AND ("PERM_GROUPS"."GRP_TRANS_NUM"="PERM_BLDG"."PERM_TRANS_NUM")) INNER JOIN "IRIS"."PARCELS" "PARCELS" ON ("PERM_GROUPS"."RPID_LOT"="PARCELS"."RPID_LOT") AND ("PERM_GROUPS"."RPID_MAP"="PARCELS"."RPID_MAP")) INNER JOIN "IRIS"."PERMIT_CENSUS_LAND_USE_CODES" "PERMIT_CENSUS_LAND_USE_CODES" ON "PERM_GROUPS"."GRP_CENSUS_LAND_USE_CODE"="PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE") INNER JOIN "IRIS"."ADDRESSES" "ADDRESSES" ON (("PERM_GROUPS"."ADDRESS_ID"="ADDRESSES"."ADDRESS_ID") AND ("PARCELS"."RPID_MAP"="ADDRESSES"."RPID_MAP")) AND ("PARCELS"."RPID_LOT"="ADDRESSES"."RPID_LOT")) INNER JOIN "IRIS"."CITY_LIMITS_CODES" "CITY_LIMITS_CODES" ON "PARCELS"."PARC_IN_OUT_CITY_LIMITS"="CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS") INNER JOIN "IRIS"."LAND_USE_CODES" "LAND_USE_CODES" ON "PARCELS"."PARC_LAND_USE"="LAND_USE_CODES"."LAND_USE_CODE"
       WHERE  ("PERM_GROUPS"."GRP_STATUS"='A' OR "PERM_GROUPS"."GRP_STATUS"='I' OR "PERM_GROUPS"."GRP_STATUS"='V' )
        and "PERM_BLDG"."PERMIT_NUM" between #{min} and #{max}
-      ORDER BY "PERM_BLDG"."PERMIT_NUM" 
+      ORDER BY "PERM_BLDG"."PERMIT_NUM"
       SQL
   end
-    
+
   def export  #process the data and send to Socrata
 
-    result_objects=@db[@sql].all    
+    result_objects=@db[@sql].all
     result_objects.each do |h|
    ############rename keys to make human friendly
    h.rewrite(:devplan_devplan_name => :development_plan_name,
             :grp_issue_date  =>    :issue_date,
             :grp_proposed_work => :proposed_work,
-            :perm_work_type_code_descr => :work_type_description, 
+            :perm_work_type_code_descr => :work_type_description,
             :permit_num => :permit_number,
             :perm_sq_ft_for_fee => :square_feet,
             :grp_bldg_num_stories => :number_of_stories,
@@ -88,28 +88,28 @@ class ConnectQuery
             :parc_county => :county,
             :in_out_city_limits_descr => :in_out_city_limits,
             :landusecode_descr => :land_use_code
-             
+
  )
 
 
     ##############set proper date_type
            h[:issue_date]=h[:issue_date].to_datetime
-              if !h[:building_date].nil? 
+              if !h[:building_date].nil?
                 h[:building_date] =  h[:building_date].to_datetime
-              end 
-              
-    ##############concatenate address fields to Location 1       
-        temp_address = {:address =>(h[:street_num].to_s << ' ' << 
-                                               h[:dir_pre].to_s << ' ' << 
-                                               h[:street_name].to_s << ' ' << 
-                                               h[:street_type].to_s << ' ' << 
-                                               h[:dir_suf].to_s << ' ' << 
-                                               h[:city].to_s <<  " NC  " << 
+              end
+
+    ##############concatenate address fields to Location 1
+        temp_address = {:address =>(h[:street_num].to_s << ' ' <<
+                                               h[:dir_pre].to_s << ' ' <<
+                                               h[:street_name].to_s << ' ' <<
+                                               h[:street_type].to_s << ' ' <<
+                                               h[:dir_suf].to_s << ' ' <<
+                                               h[:city].to_s <<  " NC  " <<
                                                h[:zip].to_s ).squeeze(' ')}
-     
+
         package = h.merge!(temp_address)
-        
-        @payload << package 
+
+        @payload << package
 
     end
       response = @client.post(@view_id, @payload)         #upload to Socrata
@@ -125,9 +125,7 @@ class ConnectQuery
       LOGGER.info "................. #{response["Rows Created"]} Rows Created"
       LOGGER.info "................. #{response["Rows Updated"]} Rows Updated"
 
-   end 
+   end
 end
- 
-ConnectQuery.new( DB, ARGV[0] ).stepper 
 
-
+ConnectQuery.new( DB, ARGV[0] ).stepper
