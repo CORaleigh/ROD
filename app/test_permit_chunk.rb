13e2cbe0cd2d803e19dee200a61a821e5b106d2c
xfinger <xfinger@gmail.com>
2015-02-12 19:31:30 -0500

    added new header fields to permit_chunk & test_permit_chunk

--- app/test_permit_chunk.rb
+++ app/test_permit_chunk.rb
@@ -6,7 +6,7 @@
 # EX. ruby permit_chunk.rb 1000  => will upload permit data with permit numbers 1000 through 11000.
 # This will query the database, concatenate the address fields, rename the column names for a more human readable table,
 # adjust the date fields and push up to 10,000 records at a time based on permit id.
- 
+
 require 'net/https'
 require 'hashie'
 require 'rubygems'
@@ -21,7 +21,7 @@ require_relative '../lib/defaults.rb'
 require_relative '../lib/helpers.rb'
 
 
-DB = Sequel.oracle( :database => 'irisdev', :host => 'ucsdevrac1', :port => 1531, :user => configatron.user_dev, :password => configatron.pass_dev)
+DB = Sequel.oracle( :database => configatron.db, :host => configatron.host, :port => 1531, :user => configatron.user, :password => configatron.pass)
 class ConnectQuery
   
   DATE_FORMAT = '%m/%d/%Y'
@@ -38,10 +38,11 @@ class ConnectQuery
       }) 
     
     @db = db
-    @view_id = '3rng-pv3r'   #permit data-set code for Socrata
+    #@view_id = '3rng-pv3r'   #permit test data-set code for Socrata
+    @view_id = 'nii6-a7pg' #permit test working copy
     @payload =[]
     @min=min                 #return permit info starting at permit number
-    @max=@min.to_i + 1000    #return permit info ending at permit number + 10,000
+    @max=@min.to_i + 50 #return permit info ending at permit number + 10,000
     @counter=0
   end
 
@@ -54,11 +55,11 @@ class ConnectQuery
   
   def get_sql(min, max)
       <<-SQL 
-      SELECT "PERM_GROUPS"."DEVPLAN_DEVPLAN_NAME", "PERM_GROUPS"."GRP_ISSUE_DATE", "PERM_GROUPS"."GRP_PROPOSED_WORK", "PERM_BLDG"."PERM_WORK_TYPE_CODE_DESCR", "PERM_GROUPS"."STREET_NUM", "PERM_GROUPS"."DIR_PRE", "PERM_GROUPS"."STREET_NAME", "PERM_GROUPS"."STREET_TYPE", "PERM_BLDG"."PERMIT_NUM", "PERM_BLDG"."PERM_SQ_FT_FOR_FEE", "PERM_GROUPS"."GRP_BLDG_NUM_STORIES", "PERM_GROUPS"."NCPIN", "PERM_BLDG"."PERM_COST_OF_CONSTRUCTION", "PERM_GROUPS"."GRP_CNTY_PARC_OWNER_NAME", "PERM_BLDG"."PERM_CONTRACTOR_NAME", "PERM_GROUPS"."RPID_LOT", "PERM_GROUPS"."PERM_C_BLDG_CO_DATE", "PERM_BLDG"."PERM_AUTHORIZED_WORK", "PERM_GROUPS"."GRP_B_NUM_DWEL_UNITS_TOTAL", "PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE_DESCR", "PARCELS"."CNTY_ACCT_NUM", "PERM_GROUPS"."GRP_STATUS", "PERM_GROUPS"."STREET_SUITE", "PARCELS"."PARC_COUNTY", "CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS_DESCR", "PERM_GROUPS"."DIR_SUF", "ADDRESSES"."CITY", "ADDRESSES"."STATE", "ADDRESSES"."ZIP", "LAND_USE_CODES"."LANDUSECODE_DESCR"
-      FROM   ((((("IRIS"."PERM_GROUPS" "PERM_GROUPS" INNER JOIN "IRIS"."PERM_BLDG" "PERM_BLDG" ON ("PERM_GROUPS"."GROUP_NUM"="PERM_BLDG"."GROUP_NUM") AND ("PERM_GROUPS"."GRP_TRANS_NUM"="PERM_BLDG"."PERM_TRANS_NUM")) INNER JOIN "IRIS"."PARCELS" "PARCELS" ON ("PERM_GROUPS"."RPID_LOT"="PARCELS"."RPID_LOT") AND ("PERM_GROUPS"."RPID_MAP"="PARCELS"."RPID_MAP")) INNER JOIN "IRIS"."PERMIT_CENSUS_LAND_USE_CODES" "PERMIT_CENSUS_LAND_USE_CODES" ON "PERM_GROUPS"."GRP_CENSUS_LAND_USE_CODE"="PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE") INNER JOIN "IRIS"."ADDRESSES" "ADDRESSES" ON (("PERM_GROUPS"."ADDRESS_ID"="ADDRESSES"."ADDRESS_ID") AND ("PARCELS"."RPID_MAP"="ADDRESSES"."RPID_MAP")) AND ("PARCELS"."RPID_LOT"="ADDRESSES"."RPID_LOT")) INNER JOIN "IRIS"."CITY_LIMITS_CODES" "CITY_LIMITS_CODES" ON "PARCELS"."PARC_IN_OUT_CITY_LIMITS"="CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS") INNER JOIN "IRIS"."LAND_USE_CODES" "LAND_USE_CODES" ON "PARCELS"."PARC_LAND_USE"="LAND_USE_CODES"."LAND_USE_CODE"
-      WHERE  ("PERM_GROUPS"."GRP_STATUS"='A' OR "PERM_GROUPS"."GRP_STATUS"='I' OR "PERM_GROUPS"."GRP_STATUS"='V' )
-       and "PERM_BLDG"."PERMIT_NUM" between #{min} and #{max}
-      ORDER BY "PERM_BLDG"."PERMIT_NUM" 
+        SELECT "PERM_GROUPS"."DEVPLAN_DEVPLAN_NAME","PERM_GROUPS"."GRP_ISSUE_DATE","PERM_GROUPS"."GRP_PROPOSED_WORK","PERM_BLDG"."PERM_WORK_TYPE_CODE_DESCR","PERM_GROUPS"."STREET_NUM","PERM_GROUPS"."DIR_PRE","PERM_GROUPS"."STREET_NAME","PERM_GROUPS"."DIR_SUF","PERM_GROUPS"."STREET_TYPE","ADDRESSES"."CITY", "ADDRESSES"."STATE", "ADDRESSES"."ZIP","LAND_USE_CODES"."LANDUSECODE_DESCR", "PARCELS"."CNTY_ACCT_NUM","PERM_BLDG"."PERMIT_NUM","PERM_BLDG"."PERM_SQ_FT_FOR_FEE","PERM_GROUPS"."GRP_BLDG_NUM_STORIES","PERM_GROUPS"."NCPIN","PERM_BLDG"."PERM_COST_OF_CONSTRUCTION","PERM_GROUPS"."GRP_CNTY_PARC_OWNER_NAME","PERM_GROUPS"."GRP_CNTY_PARC_OWNER_ADDR1","PERM_GROUPS"."GRP_CNTY_PARC_OWNER_ADDR2","PERM_BLDG"."PERM_CONTRACTOR_NAME","PERM_GROUPS"."RPID_LOT","PERM_GROUPS"."PERM_C_BLDG_CO_DATE","PERM_BLDG"."PERM_AUTHORIZED_WORK","PERM_GROUPS"."GRP_B_NUM_DWEL_UNITS_TOTAL","PERM_GROUPS"."GRP_BLDG_FOOTPRINT","CONTRACTORS"."CONTRACTOR_E_MAIL","CONTRACTORS"."CONTRACTOR_PHONE","CONTRACTORS"."CONTRACTOR_ID","PERM_GROUPS"."GRP_TENANT_NAME","PERM_GROUPS"."GRP_STATUS", "PERM_GROUPS"."STREET_SUITE", "PARCELS"."PARC_COUNTY", "CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS_DESCR", "PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE_DESCR","PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE"
+        FROM   ((((("IRIS"."PERM_GROUPS" "PERM_GROUPS" INNER JOIN "IRIS"."PERM_BLDG" "PERM_BLDG" ON ("PERM_GROUPS"."GROUP_NUM"="PERM_BLDG"."GROUP_NUM") AND ("PERM_GROUPS"."GRP_TRANS_NUM"="PERM_BLDG"."PERM_TRANS_NUM")) INNER JOIN "IRIS"."PARCELS" "PARCELS" ON ("PERM_GROUPS"."RPID_LOT"="PARCELS"."RPID_LOT") AND ("PERM_GROUPS"."RPID_MAP"="PARCELS"."RPID_MAP")) INNER JOIN "IRIS"."PERMIT_CENSUS_LAND_USE_CODES" "PERMIT_CENSUS_LAND_USE_CODES" ON "PERM_GROUPS"."GRP_CENSUS_LAND_USE_CODE"="PERMIT_CENSUS_LAND_USE_CODES"."CENSUS_LAND_USE_CODE") INNER JOIN "IRIS"."CONTRACTORS" "CONTRACTORS" ON "PERM_BLDG"."PERM_CONTRACTOR_ID"="CONTRACTORS"."CONTRACTOR_ID"INNER JOIN "IRIS"."ADDRESSES" "ADDRESSES" ON (("PERM_GROUPS"."ADDRESS_ID"="ADDRESSES"."ADDRESS_ID") AND ("PARCELS"."RPID_MAP"="ADDRESSES"."RPID_MAP")) AND ("PARCELS"."RPID_LOT"="ADDRESSES"."RPID_LOT")) INNER JOIN "IRIS"."CITY_LIMITS_CODES" "CITY_LIMITS_CODES" ON "PARCELS"."PARC_IN_OUT_CITY_LIMITS"="CITY_LIMITS_CODES"."IN_OUT_CITY_LIMITS") INNER JOIN "IRIS"."LAND_USE_CODES" "LAND_USE_CODES" ON "PARCELS"."PARC_LAND_USE"="LAND_USE_CODES"."LAND_USE_CODE"
+        WHERE  ("PERM_GROUPS"."GRP_STATUS"='A' OR "PERM_GROUPS"."GRP_STATUS"='I' OR "PERM_GROUPS"."GRP_STATUS"='V' )
+        AND "PERM_BLDG"."PERMIT_NUM" between #{min} and #{max}
+        ORDER BY "PERM_BLDG"."PERMIT_NUM" 
       SQL
   end
     
@@ -78,40 +79,58 @@ class ConnectQuery
             :perm_cost_of_construction => :cost_of_construction,
             :grp_cnty_parc_owner_name => :owner_name,
             :perm_contractor_name => :contractor_name,
-            :rpid_lot => :rpid_lot,
-            :perm_c_bldg_co_date => :building_date,
+            :contractor_id => :contractor_city_id,
+            :contractor_e_mail => :contractor_email,
+            :rpid_lot => :lot_number,
+            :perm_c_bldg_co_date => :building_co_date,
             :perm_authorized_work => :authorized_work,
-            :grp_b_num_dwel_units_total => :dwelling_units_total,
-            :census_land_use_code_descr => :land_use_code_description,
+            :grp_b_num_dwel_units_total => :dwelling_units_total, 
+            :grp_bldg_footprint => :building_footprint,
             :cnty_acct_num => :county_account_number,
             :grp_status => :status,
             :parc_county => :county,
             :in_out_city_limits_descr => :in_out_city_limits,
-            :landusecode_descr => :land_use_code
-             
+            :census_land_use_code_descr => :land_use_code_description,
+            :landusecode_descr => :land_use_code            
  )
 
 
     ##############set proper date_type
            h[:issue_date]=h[:issue_date].to_datetime
-              if !h[:building_date].nil? 
-                h[:building_date] =  h[:building_date].to_datetime
+              if !h[:building_co_date].nil? 
+                h[:building_co_date] =  h[:building_co_date].to_datetime
               end 
-              
-    ##############concatenate address fields to Location 1       
+    ##############concatenate owner address fields
+        @owner_add = ''
+        unless h[:grp_cnty_parc_owner_addr1].nil?  
+          @owner_add = h[:grp_cnty_parc_owner_addr1]
+            if !h[:grp_cnty_parc_owner_addr2].nil?  || h[:grp_cnty_parc_owner_addr1].include?( h[:grp_cnty_parc_owner_addr1])
+              @owner_add = @owner_add + ' ' + h[:grp_cnty_parc_owner_addr2]
+            end
+            o_address = {:owners_address => @owner_add}
+            package = h.merge!(o_address)
+        end
+    ##############concatenate address fields to Location 1 
+          if !h[:street_suite].nil?       #add suite to full address unless nil
+            @suite = 'STE' + ' ' + h[:street_suite].to_s + ' ' 
+          else
+            @suite = ''
+          end
+                                    
         temp_address = {:address =>(h[:street_num].to_s << ' ' << 
-                                               h[:dir_pre].to_s << ' ' << 
-                                               h[:street_name].to_s << ' ' << 
-                                               h[:street_type].to_s << ' ' << 
-                                               h[:dir_suf].to_s << ' ' << 
-                                               h[:city].to_s <<  " NC  " << 
-                                               h[:zip].to_s ).squeeze(' ')}
+                                    h[:dir_pre].to_s << ' ' << 
+                                    h[:street_name].to_s << ' ' << 
+                                    h[:street_type].to_s << ' ' << 
+                                    h[:dir_suf].to_s << ' ' << 
+                                    @suite << ' ' <<
+                                    h[:city].to_s <<  " NC  " << 
+                                    h[:zip].to_s ).squeeze(' ')}
      
         package = h.merge!(temp_address)
         
         @payload << package 
-        @counter+=1
 
+        @counter+=1
     end
       response = @client.post(@view_id, @payload)         #upload to Socrata
 
